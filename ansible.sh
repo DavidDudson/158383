e44c881ef0afc0a76c1659de99d32c10b17270e3
JureB1 <41538013+JureB1@users.noreply.github.com>
2018-08-23 23:55:33 +1200

    Add files via upload

---
+++ ansible.sh
@@ -0,0 +1,21 @@
+# !/usr/bin/env bash
+# Script for installing the required software to the master machine
+# Set Up Awscli then run the playbook
+
+# Install Python
+sudo apt update                 # Run Update
+sudo apt-get install python     # Install Python
+
+# Install Ansible
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
+
+# Install AWSCLI
+sudo apt install awscli                      # Install AWSCLI
+aws configure                                # Open AWS configure file for adding the AWS key
+
+ansible-playbook -i ./hosts wordpress.yml    # Run the PlayBook
+
