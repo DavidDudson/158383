12e6e0cfa1bfe71ccfd9cb1eca8a17ac47e000e6
JureB1 <41538013+JureB1@users.noreply.github.com>
2018-08-27 15:40:13 +1200

    Add files via upload

---
+++ task2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script for installing the required software to the master machine, Set Up Awscli and then run the playbook
+
+# Install Python
+apt -y update                 # Run Update
+apt-get -y install python     # Install Python
+apt install python-pip -y     # pip
+pip install boto              # python boto module
+pip install boto3             # python boto3 module
+
+# Install Ansible
+apt-get -y update
+apt-get -y install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get -y update
+apt-get -y install ansible
+
+#Install AWSCLI
+apt install -y awscli                      # Install AWSCLI
+aws configure                              # For adding AWS key
+
+cd ansible
+sudo ansible-playbook -i ./hosts site.yml   # Run the PlayBook
+
+echo "All done!!"                           # Display the IP address        
+echo "Copy and paste the IP address shown below into your Web Browser to access Wordpress:"
+curl ipecho.net/plain ; echo
